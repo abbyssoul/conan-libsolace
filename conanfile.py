12487859d388e3c9f61f4d2062c04c1e1bc664da
abbyssoul <abbyssoul@gmail.com>
2019-01-06 14:22:27 +1100

    Fixed test invocation

--- conanfile.py
+++ conanfile.py
@@ -11,7 +11,7 @@ class LibsolaceConan(ConanFile):
     version = "0.1.1"
     license = "Apache-2.0"
     author = "Ivan Ryabov <abbyssoul@gmail.com>"
-    url = "https://github.com/abbyssoul/conan-%s" % name
+    url = "https://github.com/abbyssoul/conan-%s.git" % name
     homepage = "https://github.com/abbyssoul/%s" % name
     description = "High performance components for mission critical applications"
     topics = ("HPC", "High reliability", "P10", "solace", "performance", "c++", "conan")
@@ -20,23 +20,21 @@ class LibsolaceConan(ConanFile):
     default_options = "shared=False", "fPIC=True"
     generators = "cmake"
 
-    _src_url = "https://github.com/abbyssoul/%s" % name
-
     def config_options(self):
         if self.settings.os == "Windows":
             del self.options.fPIC
 
     def source(self):
 #        git = tools.Git()
-#        git.clone(self._src_url)
+#        git.clone(self.homepage)
         # TODO: Only clone tagged vesion: tags/self.version
-        self.run("git clone --depth 1 --recurse-submodules {}".format(self._src_url))
+        self.run("git clone --depth 1 --recurse-submodules {}".format(self.homepage))
 
     def build(self):
         cmake = CMake(self, parallel=True)
         cmake.configure(source_folder=self.name)
         cmake.build()
-        cmake.test()
+        cmake.test(target="test_solace")
         cmake.install()
 
     def package(self):
