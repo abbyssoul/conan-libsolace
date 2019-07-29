0ce44afa18f2d467ae097fbf6bb1b5d9fc92efaf
abbyssoul <abbyssoul@gmail.com>
2019-07-29 23:08:56 +1000

    Fixed libcxx variations

--- build.py
+++ build.py
@@ -6,5 +6,5 @@ from cpt.packager import ConanMultiPackager
 
 if __name__ == "__main__":
     builder = ConanMultiPackager()
-    builder.add_common_builds(shared_option_name="libsolace:shared")
+    builder.add_common_builds(shared_option_name="libsolace:shared", pure_c=False)
     builder.run()
